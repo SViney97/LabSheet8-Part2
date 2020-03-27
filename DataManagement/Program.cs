5a6ac7ca97262c594d4501e7e08ed186c0a573c6
sviney <sviney@users.noreply.github.com>
2020-03-27 16:48:07 +0000

    Add project files.

---
+++ DataManagement/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LabSheet8_Part2;
+
+namespace DataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameData db = new GameData();
+
+            using (db)
+            {
+                ComputerGame CG1 = new ComputerGame()
+                {
+                    GameID = 1,
+                    GameName = "Zelda",
+                    GameConsole = "Switch"
+
+                };
+
+                Character C1 = new Character()
+                {
+                    CharacterID = 1,
+                    CharacterName = "Link",
+                    GameID = 1,
+                    ComputerGame = CG1
+                };
+
+                ComputerGame CG2 = new ComputerGame()
+                {
+                    GameID = 2,
+                    GameName = "MarioKart",
+                    GameConsole = "Switch"
+
+                };
+
+                Character C2= new Character()
+                {
+                    CharacterID = 2,
+                    CharacterName = "Princess Peach",
+                    GameID = 2,
+                    ComputerGame = CG2
+                };
+                Character C3 = new Character()
+                {
+                    CharacterID = 3,
+                    CharacterName = "Princess Daisy",
+                    GameID = 2,
+                    ComputerGame = CG2
+                };
+
+                db.ComputerGames.Add(CG1);
+                db.ComputerGames.Add(CG2);
+
+                Console.WriteLine("Added to DB");
+
+                db.Characters.Add(C1);
+                db.Characters.Add(C2);
+                db.Characters.Add(C3);
+                Console.WriteLine("Added characters to DB");
+
+                db.SaveChanges();
+                Console.WriteLine("Saved");
+            }
+        }
+    }
+}
---
+++ LabSheet8-Part2/Character.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSheet8_Part2
+{
+    public class Character
+    {
+        public int CharacterID { get; set; }
+        public string CharacterName { get; set; }
+
+        public int GameID { get; set; }
+        public virtual ComputerGame ComputerGame { get; set; }
+
+    }
+
+    public class ComputerGame
+    {
+        public int GameID { get; set; }
+        public string GameName { get; set; }
+        public string GameConsole { get; set; }
+
+        public virtual List<Character> Characters { get; set; }
+    }
+
+    public class GameData: DbContext
+    {
+        public GameData() : base("MyGameData") { }
+        public DbSet<Character>Characters{ get; set; }
+        public DbSet<ComputerGame>ComputerGames{ get; set; }
+    }
+}
